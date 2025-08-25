1dce0bcea49b43489e1fa083d15c47bb4b4333ad
prince0018 <sharma18.prince@gmail.com>
2025-08-25 23:45:10 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   base: '/prince3', // Use your repo name with trailing slash
 })
